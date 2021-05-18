9466b66ff67fb8b274a6bf97fee076c7a6f51375
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-05-18 18:09:30 +0200

    method to save list of students to xml added

--- src/main/java/parser/ObjectToXML.java
+++ src/main/java/parser/ObjectToXML.java
@@ -1,4 +1,22 @@
 package parser;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import entity.Student;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class ObjectToXML {
+
+    public static void saveStudentToXML(List<Student> entity) {
+        ObjectMapper mapper = new XmlMapper();
+        try {
+            mapper.writeValue(new File("studentsOut.xml"), entity);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
