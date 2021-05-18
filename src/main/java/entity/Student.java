45422626e6cfac9a2bb8332f84b3206bf7ee817d
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-05-18 18:28:44 +0200

    RequiredArgsConstructor added

--- src/main/java/entity/Student.java
+++ src/main/java/entity/Student.java
@@ -2,9 +2,11 @@ package entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class Student {
     String name;
     String surname;
