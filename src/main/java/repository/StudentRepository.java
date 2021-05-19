e95ef26d6bebe511ba1eab1509799cf6a5ec7c7f
Adam-Karzkowiak <adam.karzkowiak@gmail.com>
2021-05-19 22:55:02 +0200

    simple repository substitute added

---
+++ src/main/java/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentRepository {
+    public static List<Student> studentList = new ArrayList<>();
+}
